c4301c24cf157881e7bd1d67843eb68bfc8ef89a
Fernanda Broch <fernanda.broch@sap.com>
2024-07-29 23:31:41 -0300

    feat: category endpoint

---
+++ src/main/java/com/br/shampay/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.br.shampay.controller;
+
+import com.br.shampay.entities.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("categories")
+public class CategoryController {
+
+    @GetMapping
+    public List<Category> getCategories() {
+        return Arrays.asList(Category.values());
+    }
+}
+
